ff2f74bb94f9a04cd633396a1533924f17d74ced
lawrence <lawrence.liu@bytesforce.com>
2023-11-16 18:13:16 +0800

    init

---
+++ javassist-agent/src/test/java/com/github/mrlawrenc/JavassistAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrlawrenc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavassistAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
